93a760b64bdd5e7a041c5848c66d12a82a008b27
Krishna Kanhaiya <25430514+krishnaarga@users.noreply.github.com>
2021-08-20 21:31:26 +0530

    Arrange changable code

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php
 
-// FCM token / device id
+// Don't touch
+$url = 'https://fcm.googleapis.com/fcm/send';
+
+// Firebase Token (changeable)
+$api_key = 'AAAAYmYiF3U:APA91bF9r4cRIwRZ8LxcHEuvha5bxiLKN1aXhrSnBveDfbxcSIMaN4gc9btgAag7Lfo-_IP8T7SGqhct8yQr3SuyZpzA5kdVfst3EDEtmw117XQRN2-Ncm1JhJTL5_s';
+
+// FCM token / device id (changeable)
 $to = 'caXj-Y9HSJunLQ_iJKpwVW:APA91bHPdYl14bgP1za-uhdmcPRkEEi0sI65BMkPpvysa615Bs7n-OJ68LDlVieigd-wyOi3bWehkTX3n4DrPvPTU6ip4MBFQR6F6fs6qIe9StSC9OM6fUKQ01-5LinAG46xH071QZzl';
-// Notification
+
+// Notification (changeable)
 $data = array(
     'title'=>'Greeting',
     'body'=>'Hi from php script'
 );
-    
-// Firebase Token
-$api_key = 'AAAAYmYiF3U:APA91bF9r4cRIwRZ8LxcHEuvha5bxiLKN1aXhrSnBveDfbxcSIMaN4gc9btgAag7Lfo-_IP8T7SGqhct8yQr3SuyZpzA5kdVfst3EDEtmw117XQRN2-Ncm1JhJTL5_s';
 
-$url = 'https://fcm.googleapis.com/fcm/send';
+// Don't touch
 $fields = json_encode(array('to'=>$to, 'notification'=>$data));
-
 $ch = curl_init();
-
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POST, 1);
